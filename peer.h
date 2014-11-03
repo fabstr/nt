57e4252fab8131387935c2c6644f2cac9af4a313
Fabian Ström <fs@fabianstrom.se>
2014-11-03 23:04:19 +0100

    in progress

---
+++ peer.h
@@ -0,0 +1,60 @@
+#ifndef PEER_H
+#define PEER_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include "net.h"
+
+typedef struct message message;
+struct message {
+	int priority;
+
+	int length_prefix; // must be set
+	enum {
+		KEEPALIVE=-1, CHOKE=0, UNCHOKE=1, INTERESTED=2, NOTINTERESTED=3,
+		HAVE=4, BITFIELD=5, REQUEST=6, PIECE=7, CANCEL=8, PORT=9
+	} id;
+	unsigned char messageid; // -1 if not set
+	void *payload; // NULL if not set
+	size_t payloadlen; // 0 if payload is not set
+};
+
+typedef struct peer peer;
+struct peer {
+	char *host;
+	char *port;
+	int sockfd;
+
+	message *messageQueue;
+	size_t nmessages;
+
+	int am_choking;
+	int am_interested;
+	int peer_choking;
+	int peer_interested;
+};
+
+message *newKeepAlive();
+message *newChoke();
+message *newUnchoke();
+message *newInterested();
+message *newNotInterested();
+message *newHave(uint32_t pieceIndex);
+message *newBitfield(unsigned char *bytes, size_t nbytes);
+message *newRequest(uint32_t pieceIndex, uint32_t offset, uint32_t length);
+message *newPiece(uint32_t pieceIndex, uint32_t offset, void *block, 
+		size_t blockLength);
+message *newCancel(uint32_t pieceIndex, uint32_t offset, uint32_t length);
+message *newPort(uint16_t port);
+void freeMessage(message *m);
+
+peer* connectToPeer(char *host, char *port);
+void closePeer(peer *p); // close the socket 
+void freePeer(peer *p); // free the messages and p
+
+int handshake(peer *peer, char *infohash);
+
+#endif // PEER_H
